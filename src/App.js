e8993b7558d2323696c031c9222e97dc093b7204
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-20 10:03:27 -0300

    Menu.map()

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
+        <Categories />
+        <Menu items={menuItems} />
       </section>
     </main>
   );
